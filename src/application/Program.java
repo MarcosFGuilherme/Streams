66b3abf910001d4ec04872c31d39106410d3fb90
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-21 08:36:07 -0300

    Update -  Demostrations of Streams with Pipeline.

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,32 +2,60 @@ package application;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
 
 	public static void main(String[] args) {
+		showDemoStream();
+		showDemoPipiline();
 		
+	}
+	public static void showDemoStream() {
+		System.out.println("Demo Stream basic");
+		System.out.println("===================");
 		List<Integer> list = Arrays.asList(3,4,5,10,7);
 		System.out.println("Show original all elements numbers.");
 		Stream<Integer> st1 = list.stream();
-		System.out.println(Arrays.toString(st1.toArray()));
+		System.out.println(" " + Arrays.toString(st1.toArray()));
 
 		System.out.println("Mutiply all elements for ten.");
 		st1 = list.stream().map(x-> x * 10);
-		System.out.println(Arrays.toString(st1.toArray()));
+		System.out.println(" " + Arrays.toString(st1.toArray()));
 		
 		System.out.println("Show original all elements string.");
 		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
-		System.out.println(Arrays.toString(st2.toArray()));
+		System.out.println(" " + Arrays.toString(st2.toArray()));
 		
 		System.out.println("Show original all elements numbers of iterate.");
 		Stream<Integer> st3 = Stream.iterate(0, x -> x+2);
-		System.out.println(Arrays.toString(st3.limit(10).toArray()));
+		System.out.println(" " + Arrays.toString(st3.limit(10).toArray()));
 		
 		System.out.println("Show elements numbers of Fibonacci[Fn = Fn-1 + Fn-2].");
 		Stream<Long> st4 = Stream.iterate(new Long[] {0L,1L}, p-> new Long[] {p[1], p[0]+p[1]}).map(p-> p[0]);
-		System.out.println(Arrays.toString(st4.limit(15).toArray()));
+		System.out.println(" " + Arrays.toString(st4.limit(15).toArray()));
+		
+		System.out.println();
 	}
-
+	public static void showDemoPipiline() {
+		System.out.println("Demo Pipiline");
+		System.out.println("==============");
+		List<Integer> list = Arrays.asList(3,4,5,10,7);
+		System.out.println("Show original all elements numbers.");
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
+		System.out.println(" " + Arrays.toString(st1.toArray()));
+		
+		System.out.println("Sum of my original list.");
+		int sum = list.stream().reduce(0, (x,y) -> x+y);
+		System.out.println(" Sum = " +  sum);
+		
+		System.out.println("New list from original list with new parameters.");
+		List<Integer> newList = list.stream()
+				.filter(x-> x%2 == 0)
+				.map(x -> x * 10)
+				.collect(Collectors.toList());
+		
+		System.out.println(" " + Arrays.toString(newList.toArray()));
+	}
 }
